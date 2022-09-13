b97635a19da4ae52b6f832452284a29127f53a49
Hanna Ruter <hanna.ruter@gmail.com>
2022-09-13 13:05:37 +0200

    update react version

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App'
-import './index.css'
+import { createRoot } from 'react-dom/client';
 
+import { App } from './App';
+import './index.css';
 
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 
-
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
